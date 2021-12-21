11f3aac3c545770d30e264ea2d105b50a6055fcb
Nivas-Patil <nivaspatil9957@gmail.com>
2021-12-21 09:23:21 +0530

    initial commit

---
+++ Names.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Names = () => {
+    return (
+        <div>
+            <h1>Nivas Patil Software engineer</h1>
+        </div>
+    )
+}
+
+export default Names
